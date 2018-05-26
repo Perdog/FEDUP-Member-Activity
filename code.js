92c6976e6bf9eb27df7f7f21320bf238e57e0c1b
Perdog <jacob_perry@hotmail.ca>
2018-05-26 12:56:56 -0400

    Table sorting

--- code.js
+++ code.js
@@ -269,7 +269,8 @@ function lookupDone() {
 						"</tr>";
 	}
 	
-	$('#inactive-table').append(tableText);
+	$('#inactive-table').find('tbody').append(tableText);
+	sortTable();
 	$('#inactive-table').show();
 	console.log(list);
 	
@@ -334,5 +335,40 @@ function tickTimer() {
 
 function getCharName(id) {
 	return id;
+}
+
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("inactive-table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[3];
+      y = rows[i + 1].getElementsByTagName("TD")[3];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
 }
 
